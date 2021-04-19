c8fc30a2d21d077c135b323bf96eb9e563205858
Ivan Mitrevski <mitrevski.ivan@hotmail.com>
2021-04-19 20:47:49 +0200

    Creating my two primary components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieListComponent } from './components/movie-list/movie-list.component';
+import { FilterComponent } from './components/filter/filter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieListComponent,
+    FilterComponent
   ],
   imports: [
     BrowserModule,
